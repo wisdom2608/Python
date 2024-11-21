fc839ca26493c2083b96ae899b398b94e08816c9
wisdom2608 <mueatech087@gmail.com>
2024-11-21 11:41:23 +0100

    added new note-02

---
+++ README.md
@@ -0,0 +1,5 @@
+Python is one of the most popular languages and everyday, people use it to do cool things like:
+- Authomation
+- A.I. as well as as using it to build  application such as 
+- Websites
+- Instagram etc.
---
+++ note-02.py
@@ -0,0 +1,395 @@
+# ==============================================================================================
+# Run your first python program. With this piece of code, you can print your name on the screen.
+# ==============================================================================================
+print("wisdom python")
+
+# ====================
+# How to draw a dog
+# ===================
+
+print("0----")
+print(" ||||")
+# Description: What we have understand in the codes above is that python codes get executed line by line from the top.
+# ...we saw python enterpreter, that is the program that knows how to translate or interpret a python code into....
+# ...instructions that a computer can understand. 
+
+# ======================================
+# How to multiply a string by any number
+# ======================================
+ 
+print("*" * 10)
+
+# Description: Aywhere we have quotations "" or '', we are defining a string. A string is a programming term which means...
+# ...which means a series of characters. So, "Wisdom python", "0----",  " ||||", "*", are all strings.
+# ("*" * 10), means we are multiplying the string "*" by number, 10. The asterek is the multiplication operator just like the ...
+# ...operator we have in math.  With this piece of code, ("*" * 10), we call draw 10 astereks on the terminal.
+# What we have <"*" * 10>, on the the piece of code, print("*" * 10) is called an expression. An expression is a piece of code that ...
+# ... produces a value. So, when python interpreter wants to print a code, it evaluates the code we put in the parenthesis. 
+#  Here, ten astereks will be printed on the terminal..
+
+
+# =================
+#       Variables 
+# =================
+price = 10 
+print(price)
+# Description: Variables in python are one of the most fondamental concepts in programming, specifically to python.
+# We use variables to temporary store data in a computer's memory in binaries (a serie of 1s, and 0s).
+# when python interpreter executes this code, it will allocate some memory that will store a number, 10 in that memory.
+# Finally, it will attach the label "price" in that memory location. Imagine we have a box numbered 10, and "price" is the label we put..
+#.. in the box. We can use the labe everywhere in the program to access the value that we have in that box.
+# To print the value of the variable, we type <print(price)> without quotation in the parenthesis. This because we want to print a variable, "price".
+# This will print the number, 10 on our terminal.
+
+# ===================================
+#   Update the value of Variable
+# ===================================
+
+price = 10 
+price = 20
+print(price)
+
+# Description: If we reset it to a new value like 20. When we run the program, we should see 20.
+# Remember, python interpreter execute a code line by line from the top.
+
+# ====================================================
+#  Variables With Integers and Floating Point Numbers
+# ===================================================
+price  = 10 
+rating = 4.9
+print(price)
+
+# Description: The numbers 10 in the piece of code above is called integer. It is a number without a decimal point. we can also use number...
+# with decimal point such as 4.9, 2.5, etc, they are called floating point numbers.
+
+# ===================================================
+#  Variable With String, And Variable With Boolean
+# ====================================================
+name = "Wisdom"
+is_published = True 
+is_in_school = False
+print(name)
+print(is_published)
+print(is_in_school)
+
+# Description: We can also define a variable and set it to a string. For instance name ="Wisdom"
+# A boolean can be <True>, or <False> but not like "Yes" and "No" in English Language. We use underscore(_) to separate
+#..multiple words in a variable's name. For instance "is_published". The False and True are called boolean values.
+# NB: Python is case-sensitive language which is sensitive to lowercase and uppercase letters. So, also use lowercase letters when defining variables.
+# But the boolean True, and False are special key words in python language. So pyth will not understand it when spell "true" istead of "True", or "false" instead of "False"
+# Summary: This exercise we are storing simple values in a computer's memory. Simple values can be numers (such as integers, floats), string, and booleans.
+
+# In python we can also store complex values like <lists>, and <objects>.
+
+
+# Proof Concept: Imagine we want to write a program for a hospital. So, we check in a patient named Wisdom Faith. He's 20 years old and is a new patient
+# Work Required
+# Define:
+# - three variables for his name, his age and another variable to tell if it's a new and existing patient.
+
+#          Solution
+name = "Wisdom Faith"
+age = 20
+is_new =True
+
+
+
+# ===================================
+# How to Receive input from the user
+# ===================================
+name = input("What is your name? ")
+print("Hi " + name)
+# Description: We going to write a small program that asks the username and then will print a greeting message customized for that user.
+# So, instead of <print>, we will use <input>. Both these <input>, and <print> are functions that built into python. As a matter of fact, think of the remote control..
+#...of your TV. On this device, we have a bunch of bottons. These bottons are function built into your TV. You can turn it on, turn it of, change channel, reduce the volume, etc.
+# In python we also have a bunch of functions for common task such printing message, receiving inputs, and so on. We are going to use the <input> function and whenever we have..
+# ...parenthesis, it's like recalling what executes that function. It's like princing a botton on a remote control. 
+# Let's call an <input> and in between parenthesis we are going add a string, enabling it to print something on the terminal. ie input("What is your name? ").
+# This contains a question mark (?), followed by a space. That is, input("What is your name? ")
+# Ths <input> function will print the message < What is your name? > on the terminal and will wait for the user to enter a value. Whatever the user enters, the <input> function will return.
+# We can get that value and store it in a memory using a variable. So get the result and store it in variable called name.
+
+# On the second line of our code we want to print a message like "Hi Wisdom Faith!". To do this we type print("Hi ") with a space and after the quotation, we...
+#... want to dynamically print what we have in the < name variable >. That is print("Hi " + name)
+
+
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
+
+# ===================================
+# 
+# ===================================
